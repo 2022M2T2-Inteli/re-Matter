ffd3e54cf46ec7f0d89f68a2a4c21022e16922cf
Filipi Enzo Siqueira Kikuchi <filipikikuchi@gmail.com>
2022-05-11 16:26:25 -0300

    Implemented script to index.html

--- client/src/script.js
+++ client/src/script.js
@@ -1,6 +1,5 @@
 function movePageTo(id){
     document.getElementById("navbarSupportedContent").classList.remove("show");
-
     setTimeout(function(){
         document.getElementById(id).scrollIntoView({block: "start", behavior: "smooth"});
     }, 1);
