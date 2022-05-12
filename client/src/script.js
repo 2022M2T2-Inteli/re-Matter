05aacb4c6a6e477507f767b2b8b0fc1139d5fe15
marcelofeitoza <feitoza.marcelo7@gmail.com>
2022-05-12 15:07:34 -0300

    navbar, home, and maps

--- client/src/script.js
+++
@@ -1,6 +0,0 @@
-function movePageTo(id){
-    document.getElementById("navbarSupportedContent").classList.remove("show");
-    setTimeout(function(){
-        document.getElementById(id).scrollIntoView({block: "start", behavior: "smooth"});
-    }, 1);
-}
