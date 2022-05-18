14036eebbc0ac14fca1f1ecc96d6746d99f3d012
marcelofeitoza <feitoza.marcelo7@gmail.com>
2022-05-18 15:09:13 -0300

    Pushing changes to import methods in HTML files

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.static("../client"));
-app.use(express.static("../client/src/images"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../docs/index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`App running on ${port}`);
+  console.log(`App running on http://127.0.0.1:${port}/`);
 });
