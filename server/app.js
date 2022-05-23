ce911177eb0f3b3be258e67b7fbe381401ca99f2
marcelofeitoza <feitoza.marcelo7@gmail.com>
2022-05-23 09:36:20 -0300

    assets working

--- server/app.js
+++ server/app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const router = require("./router");
+const path = require("path");
 
 const app = express();
 
 const port = 3000;
 
-app.use(express.static("../client"));
+app.use(express.static("../"));
 
-app.use('/', router);
+app.use("/", router);
+
 
 app.get("/doacao", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/src/screens/donate.html"));
 });
 
 app.listen(port, () => {
+  console.log(path.join(__dirname, "../"));
   console.log(`App running on http://127.0.0.1:${port}/`);
 });
