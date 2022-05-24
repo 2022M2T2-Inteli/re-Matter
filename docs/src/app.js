ebb5a192acc6c17af7b5aba306a96704c6ea3d5e
marcelofeitoza <feitoza.marcelo7@gmail.com>
2022-05-24 10:10:22 -0300

    change sources and hrefs

--- docs/src/app.js
+++ docs/src/app.js
@@ -1,8 +1,4 @@
 import express from "express";
-
-import path from "path";
-const __dirname = path.resolve(path.dirname(""));
-
 import { openDb } from "./configDB.js";
 import { createRecordDatabase } from "./Controller/Ficha.js";
 
@@ -21,13 +17,7 @@ createRecordDatabase();
 openDb();
 
 app.use("/", router);
-
-// provisorio
-app.use("/area-restrita/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../docs/Views/area-restrita.html"));
-});
-
-app.use("/area-restrita/fichas", recordsRouter);
+app.use("/area-restrita/", recordsRouter);
 
 app.listen(PORT, () =>
   console.log(`Server running on port http://127.0.0.1:${PORT}`)
