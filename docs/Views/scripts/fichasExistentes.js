f1c4595fdb469977e1288cec276ade931a6b565b
joaoalca <joao.alcaraz@sou.inteli.edu.br>
2022-06-21 15:53:40 -0300

    identation

--- docs/Views/scripts/fichasExistentes.js
+++ docs/Views/scripts/fichasExistentes.js
@@ -16,15 +16,15 @@ const getAssisteds = () => {
 
       renderAssisted(assisteds);
       //função que escreve o relatório de assistidos
-      
-      $("#but").click(function(){
-          console.log("entrou")
-          function generateRelatory() {
-            let relatory = "";
-            //forma um relatorio da tabela dos assistidos
-            for (let info in assisteds) {
-              //adiciona ao relatorio as informações do assistido
-              relatory += `
+
+      $("#but").click(function () {
+        console.log("entrou")
+        function generateRelatory() {
+          let relatory = "";
+          //forma um relatorio da tabela dos assistidos
+          for (let info in assisteds) {
+            //adiciona ao relatorio as informações do assistido
+            relatory += `
               Nome: ${assisteds[info].name}
               Nome social: ${assisteds[info].nickname}
               Data de chegada: ${assisteds[info].approachDate}
@@ -33,22 +33,22 @@ const getAssisteds = () => {
               Responsável: ${assisteds[info].responsibleId}
               \n
             `
-            }
-            //teste
-            console.log(assisteds.find(assisted => assisted.createdAt == "6/9/2022"))
-            console.log(relatory)
-            //retorna o relatorio
-            return relatory;
           }
-          //cria um pdf
-          var doc = new jsPDF({
-            orientation: 'portrait',
-            unit: 'cm',
-            format: 'A4'
-          })
-          //adiciona o relatorio ao pdf
-          doc.text(generateRelatory(), 1, 1)
-          doc.save('relatorio.pdf')   
+          //teste
+          console.log(assisteds.find(assisted => assisted.createdAt == "6/9/2022"))
+          console.log(relatory)
+          //retorna o relatorio
+          return relatory;
+        }
+        //cria um pdf
+        var doc = new jsPDF({
+          orientation: 'portrait',
+          unit: 'cm',
+          format: 'A4'
+        })
+        //adiciona o relatorio ao pdf
+        doc.text(generateRelatory(), 1, 1)
+        doc.save('relatorio.pdf')
       })
       return assisteds;
     })
